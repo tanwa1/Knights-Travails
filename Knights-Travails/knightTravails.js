97961f60073eba9a4bac0ea9e43b7990566776a2
tanwa1 <73098650+tanwa1@users.noreply.github.com>
2026-09-03 22:43:59 +0800

    added getKnightMoves() function

--- Knights-Travails/knightTravails.js
+++ Knights-Travails/knightTravails.js
@@ -1,20 +1,39 @@
-export function isValidSquare([x, y]){
-    return x >= 0 && x <= 7 && y >= 0 && y <= 7;
+export function isValidSquare([x, y]) {
+  return x >= 0 && x <= 7 && y >= 0 && y <= 7;
 }
 
-export function getKnightMoves(square){
+export function getKnightMoves(square) {
+  const x = square[0];
+  const y = square[1];
 
+  const moves = [];
+
+  const eightMoves = [
+    [1, 2],
+    [2, 1],
+    [2, -1],
+    [1, -2],
+    [-1, -2],
+    [-2, -1],
+    [-2, 1],
+    [-1, 2],
+  ];
+
+  eightMoves.forEach(([dx, dy]) => {
+    const nx = x + dx;
+    const ny = y + dy;
+    if (isValidSquare([nx, ny])) moves.push([nx, ny]);
+  });
+  return moves;
 }
 
-export function knightMoves(start, end){
+export function knightMoves(start, end) {}
 
-}
-
-export function parseSquare(input){
-    if(typeof input === 'string'){
-        const x = input[0].charCodeAt(0) - 97;
-        const y = parseInt(input[1], 10) - 1;
-        return [x, y];
-    }
-    return input;
+export function parseSquare(input) {
+  if (typeof input === "string") {
+    const x = input[0].charCodeAt(0) - 97;
+    const y = parseInt(input[1], 10) - 1;
+    return [x, y];
+  }
+  return input;
 }
